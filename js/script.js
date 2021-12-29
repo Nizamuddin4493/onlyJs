509ddff12e34611c34336ccb660d0f7576f2b421
Nizamuddin4493 <nizamuddin4493@gmail.com>
2021-12-30 00:34:53 +0430

    ifAndSwitch

--- js/script.js
+++ js/script.js
@@ -24,3 +24,50 @@
 //     console.log('your guested numbers is highter then guested number.');
 //     }
 // }
+
+/*      if statement          */
+// let temp = 45;
+// if (temp <= 0) {
+//     console.log('It is too cool outsite.')
+// } else if (temp > 0 && temp <= 7) {
+//     console.log('It is cool outsite.')
+// } else if (temp >7 && temp <= 18) {
+//     console.log('It is Normal outsite.')
+// } else if (temp > 18 && temp <= 28) {
+//     console.log('It is a litte hot outsite.')
+// } else if (temp > 28 && temp <= 35) {
+//     console.log('It is hot outsite.')
+// } else {
+//     console.log('It is too hot outsite.')
+// }
+// let num1 = 4;
+// let num2 = 7;
+// let num3 = num1 >= num2 ? 'greater' : 'less then';
+
+/*      Using switch          */
+// let day = 0;
+// switch (day) {
+//     case 0:
+//         console.log('Saturday');
+//         break;
+//     case 1:
+//         console.log('Sunday');
+//         break;
+//     case 2:
+//         console.log('Monday');
+//         break;
+//     case 3:
+//         console.log('Tuesday');
+//         break;
+//     case 4:
+//         console.log('Wednesday');
+//         break;
+//     case 5:
+//         console.log('Thursday');
+//         break;
+//     case 6:
+//         console.log('Friday');
+//         break;
+//     default:
+//     console.log('Invalid day')
+// }
